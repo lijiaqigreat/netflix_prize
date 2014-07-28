73d6aa5d711b3b5ed4b1edfce72b9a3644d2bff8
Jiaqi <lijiaqigreat@gmail.com>
2014-07-28 12:51:49 -0500

    ..

--- turing_test.sh
+++ turing_test.sh
@@ -2,5 +2,5 @@ gcc allvec_new.c -lm -O3 -o allvec.out
 gcc nsvd1*.c -lm -O3 -o nsvd1.out
 gcc nsvd2*.c -lm -O3 -o nsvd2.out
 gcc nsvd3*.c -lm -O3 -o nsvd3.out
-./nsvd1.out tmp/bigfile/main.dat 123 0 1 > tmp1
-./nsvd3.out tmp/bigfile/data1.dat tmp/bigfile/data2.dat tmp/bigfile/datav.dat 1 1 1 1 > tmp3
+./nsvd1.out ../tmp/bigfile/main.dat 123 0 1 > tmp1
+./nsvd3.out ../tmp/bigfile/data1.dat ../tmp/bigfile/data2.dat ../tmp/bigfile/datav.dat 1 1 1 1 > tmp3
