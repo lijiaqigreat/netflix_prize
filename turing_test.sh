ada3e0e2ae9a04a7be421b62838de2a036a23370
Jiaqi <lijiaqigreat@gmail.com>
2014-07-28 12:46:54 -0500

    ..

--- turing_test.sh
+++ turing_test.sh
@@ -2,6 +2,5 @@ gcc allvec_new.c -lm -O3 -o allvec.out
 gcc nsvd1* -lm -O3 -o nsvd1.out
 gcc nsvd2* -lm -O3 -o nsvd2.out
 gcc nsvd3* -lm -O3 -o nsvd3.out
-./nsvd1.out tmp/bigfile/main.dat 123 0 10 > tmp1
-./nsvd3.out tmp/bigfile/main.dat 123 0 10 > tmp3
-    printf("usage: ./main.out\n path of main.dat\n random seed\n percentage of qualify\n number of training iteration\n");
+./nsvd1.out tmp/bigfile/main.dat 123 0 1 > tmp1
+./nsvd3.out tmp/bigfile/data1.dat tmp/bigfile/data2.dat tmp/bigfile/datav.dat 1 1 1 1 > tmp3
